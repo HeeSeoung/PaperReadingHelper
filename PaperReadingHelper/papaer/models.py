7eaff38bd563c99119018f0cd0c3fb3f53be5a81
jongmin4422 <jongmin4422@naver.com>
2021-11-22 16:03:12 +0900

    add paper table

--- PaperReadingHelper/papaer/models.py
+++ PaperReadingHelper/papaer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Paper(models.Model):
+    file_name = models.TextField(db_column="file_name", null=True)
+    content = models.TextField()
+    file_path = models.CharField(
+        db_column="file_path", max_length=250, null=True)
+    upload_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'paper'
