a89ebe7f8307fbb3fc47bdfd88d87a4ad8f295d5
hiseoung <anhisg4104@gmail.com>
2021-11-22 17:01:05 +0900

    update

--- PaperReadingHelper/user/urls.py
+++ PaperReadingHelper/user/urls.py
@@ -7,6 +7,7 @@ from .views import LoginView, RegisterView
 urlpatterns = [
 
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/login'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
 
 ]
