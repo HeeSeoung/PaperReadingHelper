883bd10f86d7935a2da133b1351f68463d5a3fd8
hiseoung <anhisg4104@gmail.com>
2021-11-22 15:42:17 +0900

    update

--- PaperReadingHelper/static/js/user/script_login.js
+++ PaperReadingHelper/static/js/user/script_login.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 const btnLogin = document.getElementById('login-btn');
 
 
@@ -30,6 +30,7 @@ btnLogin.addEventListener('click', async() => {
     })
     const result = await response.json()
     if (result.success){
+        alert('성공!');
         location.href='/';
     }
 
@@ -41,3 +42,20 @@ function enterkey() {
         login();
     }
 }
+
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+    return cookieValue;
+    }
