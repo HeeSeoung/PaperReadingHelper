acf1901fb74a61e0321858630851d4ffd6436fbe
hiseoung <anhisg4104@gmail.com>
2021-12-01 22:23:52 +0900

    Edit Bentoml Easyocr

--- AIServer/easyocr_bentoml.py
+++ AIServer/easyocr_bentoml.py
@@ -1,21 +1,22 @@
 import bentoml
 from bentoml.frameworks.easyocr import EasyOCRArtifact
 from bentoml.adapters import ImageInput
+import numpy as np
 
 @bentoml.env(pip_packages=["easyocr>=1.3.0"])
-@bentoml.artifacts([EasyOCRArtifact("chinese_small")])
+@bentoml.artifacts([EasyOCRArtifact("en")])
 class EasyOCRService(bentoml.BentoService):
     @bentoml.api(input=ImageInput(), batch=False)
     def predict(self, image):
-        reader = self.artifacts.chinese_small
+        reader = self.artifacts.en
         raw_results = reader.readtext(np.array(image))
         text_instances = [x[1] for x in raw_results]
         return {"text" : text_instances}
 
 import easyocr
 service = EasyOCRService()
-lang_list = ['ch_sim', 'en']
+lang_list = ['en']
 recog_network = "zh_sim_g2"
 model = easyocr.Reader(lang_list=lang_list, download_enabled=True, recog_network=recog_network)
-service.pack('chinese_small', model, lang_list=lang_list, recog_network= recog_network)
+service.pack('en', model, lang_list=lang_list, recog_network= recog_network)
 saved_path = service.save()
