8560e70bfe4e573faca74e89a01f72065fea60f9
hiseoung <anhisg4104@gmail.com>
2021-12-14 01:15:26 +0900

    update

--- PaperReadingHelper/visual/views.py
+++ PaperReadingHelper/visual/views.py
@@ -7,7 +7,6 @@ from paper import models as model
 from django.contrib.auth.models import User
 from nltk.tokenize import word_tokenize, sent_tokenize
 import nltk
-from nltk.corpus import stopwords
 import re
 from collections import Counter
 from gensim.models import Word2Vec
@@ -24,7 +23,23 @@ class VisualView(LoginRequiredMixin, View):
             print('안됨')
 
         all_text = ' '.join(data)
-        stop_words = set(stopwords.words('english'))       
+        stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're",
+                    "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his',
+                    'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they',
+                    'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that',
+                    "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being',
+                    'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and',
+                    'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with',
+                    'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above',
+                    'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again',
+                    'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all',
+                    'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor',
+                    'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will',
+                    'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 
+                    've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn',
+                    "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma',
+                    'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't",
+                    'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]     
         nltk.download('punkt')
         data = re.sub(r'\([^)]*\)', '', all_text)
         data = sent_tokenize(data)
