53856823fa9b43a00f20e1072c1454691c093106
hiseoung <anhisg4104@gmail.com>
2021-12-19 23:32:28 +0900

    Change Server Port Number

--- PaperReadingHelper/paper/views.py
+++ PaperReadingHelper/paper/views.py
@@ -95,8 +95,8 @@ class HomeView(LoginRequiredMixin, View):
                     'image_file': (f'{path}', open(f'{path}', 'rb')),
                 }
 
-                # response = requests.post('http://127.0.0.1:43051/predict', files=files)
-                response = requests.post('http://127.0.0.1:55647/predict', files=files)
+                response = requests.post('http://127.0.0.1:43051/predict', files=files)
+                # response = requests.post('http://127.0.0.1:54685/predict', files=files)
                 text = response.json()
                 text = ' '.join(text)
 
